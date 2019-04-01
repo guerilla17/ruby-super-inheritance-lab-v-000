80e1a20c1f20fe810cdf1db9fc9248272709a665
Gigi M <mmgamao1@gmail.com>
2019-04-01 18:44:00 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,9 +1,7 @@
 # •	Write a method in the ChattyStudent class, #hello, that uses the super keyword to inherit the behavior of the #hello method from the parent, Student, class.
 
-class ChattyStudent
-  attr_accessor :
+class ChattyStudent < Student 
   
-  def initialize(C)
 
 
 # •	Then, augment that method to also puts out the very chatty phrase: "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
