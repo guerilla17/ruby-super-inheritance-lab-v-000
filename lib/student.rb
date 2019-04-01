158bf2a84fd3ac181c4bf4edc90f43d6bd6f2296
Gigi M <mmgamao1@gmail.com>
2019-04-01 18:38:13 +0000

    Automatically backed up by Learn

---
+++ lib/student.rb
@@ -0,0 +1,2 @@
+# •	Write a method in the Student class, #hello, that puts out the phrase: "Hey there! I'm so excited to learn stuff."
+# •	Write a method in the Student class, raise_hand, that puts out the phrase: "Pick me!"
