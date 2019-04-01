a4aae3ec56158d9d88e901a280c53e25b5a21472
Gigi M <mmgamao1@gmail.com>
2019-04-01 18:45:53 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -9,7 +9,7 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
 
-
+# Write a method in the ChattyStudent class, raise_hand, that uses the super keyword ten times so that the method will putsout "Pick me!" ten times. It is possible to simply call super multiple times in a method.
 
   def raise_hand
     super
@@ -24,6 +24,4 @@ class ChattyStudent < Student
     super
   end
 
-
-
-# Write a method in the ChattyStudent class, raise_hand, that uses the super keyword ten times so that the method will putsout "Pick me!" ten times. It is possible to simply call super multiple times in a method.
+end
